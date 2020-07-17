5d3405fa6f0e1cbdbe04c15af035637085eebebd
chandra agung rizky <needkopi@gmail.com>
2020-07-18 02:00:40 +0700

    change controller

--- http/route-users.ts
+++ http/route-users.ts
@@ -9,18 +9,8 @@ export class RouteUser {
     let route = new Route();
     let userController = new UserController();
 
-    let index = (req: ServerRequest): any => {
-        req.respond({
-            status: 200,
-            headers: new Headers({
-                "content-type":"text/html",
-            }),
-            body:"woyyy",
-        });
-    }
-
-    route.Get(req,'/index',index(req));
-    // route.Get(req,'/hello',userController.hello(req));
+    route.Get(req,'/index',userController.index(req));
+    route.Get(req,'/hello',userController.hello(req));
   }
 
 }
